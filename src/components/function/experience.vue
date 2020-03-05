2c5b20e80d2f08b45a6327fe17fc9dfd96cda64f
zhangjijun123 <1334956059@qq.com>
2020-03-05 20:44:10 +0800

    两个mounted合一起

--- src/components/function/experience.vue
+++ src/components/function/experience.vue
@@ -88,6 +88,12 @@
           console.log(err);
         });
       }
+      this.recorder = new Recorder({
+          sampleBits: 16,         // 采样位数，支持 8 或 16，默认是16
+          sampleRate: 16000,      // 采样率，支持 11025、16000、22050、24000、44100、48000，根据浏览器默认值，我的chrome是48000
+          numChannels: 1,         // 声道，支持 1 或 2， 默认是1
+          compiling: false,       // 是否边录边转换，默认是false
+      });
     },
     data() {
       return {
@@ -105,14 +111,6 @@
         ifShowCixing:false,
         showItem:false
       }
-    },
-    mounted(){
-      this.recorder = new Recorder({
-          sampleBits: 16,         // 采样位数，支持 8 或 16，默认是16
-          sampleRate: 16000,      // 采样率，支持 11025、16000、22050、24000、44100、48000，根据浏览器默认值，我的chrome是48000
-          numChannels: 1,         // 声道，支持 1 或 2， 默认是1
-          compiling: false,       // 是否边录边转换，默认是false
-      });
     },
     methods:{
       startRecord(){
